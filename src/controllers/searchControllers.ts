0d3953a5325037a30ea4b011fe359c47e9b64966
Sandro Oliveira <70119895+sandrocod@users.noreply.github.com>
2024-07-25 20:42:22 -0300

    projeto final  1.0

--- src/controllers/searchControllers.ts
+++ src/controllers/searchControllers.ts
@@ -1,5 +1,19 @@
 import { Request, Response } from "express";
+import { Pet } from "../models/pat";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const search = (req: Request, res: Response) => {
-    //res.render('page/search')
+    let query: string = req.query.q as string
+    let list = Pet.getFromName(query)
+
+    if(!query) {
+        res.redirect('/')
+        return
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,21 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mustache from 'mustache-express'
 import path from 'path'
+import mainRoutes from './routers'
 
 dotenv.config()
 
 const server = express()
 
 server.set('view engine', 'mustache')
-server.set('views', path.join(__dirname, 'viwes'))
+server.set('views', path.join(__dirname, 'views'))
 server.engine('mustache', mustache())
 server.use(express.static(path.join(__dirname, '../public')))
 
-const PORT = process.env.PORT || 3000
+server.use(mainRoutes)
 
-server.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`)
+server.use((req, res) => {
+    res.render('pages/404')
 })
+
+server.listen(process.env.PORT)
